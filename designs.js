00359ac8525aa7d0f9cc33cc7d68bf3439198008
Corey Duke <68204160+cdukedev@users.noreply.github.com>
2020-07-16 21:47:36 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,7 +1,5 @@
-// Select color input
-// Select size input
-function makeGrid(){
-            //log height,width and table
+function makeGrid() {
+    //log height,width and table
     const height = document.getElementById("inputHeight").value;
     const width = document.getElementById("inputWidth").value;
     const table = document.getElementById("pixelCanvas");
@@ -13,16 +11,15 @@ function makeGrid(){
         //insert rows width
         for (let m = 1; m <= width; m++) {
             let td = document.createElement('td');
-            tr.appendChild(td); 
+            tr.appendChild(td);
             //adds color when click on cell
-            td.addEventListener('click',function(event) {
+            td.addEventListener('click', function(event) {
                 event.target.style.backgroundColor = document.getElementById('colorPicker').value;
-    });
+            });
+        }
     }
-  }          
 }
-document.getElementById('sizePicker').addEventListener("click", function(event){
+document.getElementById('sizePicker').addEventListener("submit", function(event) {
     event.preventDefault();
-
-makeGrid();
+    makeGrid();
 })
